a15340863038a0fc96de01c93a7ab201352e2dbb
Tim Kuminecz <tkuminecz@gmail.com>
2018-09-05 15:28:39 -0400

    add basic http caching

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const { createServer } = require('http')
+const HttpCache = require('http-cache')
 const next = require('next')
 const routes = require('./router')
 
@@ -6,9 +7,25 @@ const app = next({
   dev: process.env.NODE_ENV !== 'production',
   dir: 'src'
 })
-const handler = routes.getRequestHandler(app)
+const nextHandler = routes.getRequestHandler(app)
 const port = process.env.PORT || 3000
 
+const cache = new HttpCache({
+  ttl: 30
+})
+
+function handler (req, res) {
+  if (/^\/_next/.test(req.url)) {
+    nextHandler(req, res)
+  } else {
+    cache(req, res, () => {
+      console.log(`${req.url} not cached!`)
+      nextHandler(req, res)
+    })
+  }
+}
+
+/* start the app */
 app.prepare()
   .then(() => {
     createServer(handler).listen(port, (err) => {
